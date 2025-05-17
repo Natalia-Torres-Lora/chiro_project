8fed5838cc0e0b7bf93d1dfe415856c9bb50ee76
Natalia Torres <nataliatorres@Natas-Mac.local>
2025-05-17 14:34:16 -0400

    Added applications, users, patients ,appointments, treatmets, to Installed apps in settings.py

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps for chiro_project
+    'users',
+    'patients',
+    'appointments',
+    'treatments',
 ]
 
 MIDDLEWARE = [
